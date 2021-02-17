b412fe7ecc578c2a3c88da0043711e84c294c754
utk107 <71555644+utk107@users.noreply.github.com>
2021-02-18 01:46:21 +0530

    Update sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -2,9 +2,21 @@ const form = document.querySelector('.display-form');
 
 form.addEventListener('submit', e =>{
     e.preventDefault();
-    console.log(form.firstname.value);
-    console.log(form.lastname.value);
-    console.log(form.email.value);
-    console.log(form.phoneno.value);
-    console.log(form.rollno.value);
+//     console.log(form.firstname.value);
+//     console.log(form.lastname.value);
+//     console.log(form.email.value);
+//     console.log(form.phoneno.value);
+//     console.log(form.rollno.value);
+    var array = [];
+array.push(form.firstname.value);
+array.push(form.lastname.value);
+array.push(form.email.value);
+array.push(form.phoneno.value);
+array.push(form.rollno.value); 
+console.log("First name:",array[0]);
+console.log("Last name:",array[1]);
+console.log("email:",array[2]);
+console.log("phoneno:",array[3]);
+console.log("rollno:",array[4]);
+});
 });
